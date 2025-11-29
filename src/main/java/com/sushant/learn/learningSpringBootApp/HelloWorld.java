23f551bdcc82542d792f4bb5d46e151ead6f280c
Sushant0102 <sushantchandgude107@gmail.com>
2025-11-29 10:47:05 +0530

    project added

---
+++ src/main/java/com/sushant/learn/learningSpringBootApp/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.sushant.learn.learningSpringBootApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
---
+++ src/main/java/com/sushant/learn/learningSpringBootApp/LearningSpringBootAppApplication.java
@@ -0,0 +1,14 @@
+package com.sushant.learn.learningSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringBootAppApplication.class, args);
+
+	}
+
+}
